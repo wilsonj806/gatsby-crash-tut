0658561ffca8477e68c9945fee55f02f5cf1e01e
Wilson <wilsonj806@gmail.com>
2020-02-11 15:55:56 -0500

    Add basic pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+
+const About = () =>
+    <div>
+      <h1>About page</h1>
+      <p>baby shark doo do</p>
+    </div>
+
+export default About;
---
+++ src/pages/services.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const services = () =>
+    <div>
+      <h1>Our services</h1>
+      <p>We do stuff</p>
+      <p>It's neat</p>
+    </div>
+
+export default services;
